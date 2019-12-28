9f396380dc52b1dab9fd9bf82f61435c80ebdd8d
KleinCoding <kleincoding@gmail.com>
2019-12-28 15:58:47 -0600

    completed assignment

--- Part2/index.js
+++ Part2/index.js
@@ -1,1 +1,9 @@
-console.log("The House Always Wins!")
+// console.log ('The house always wins!')
+
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+function setCard() {
+  const card = document.getElementById(idInput.value);
+  card.style.color = colorInput.value;
+}
